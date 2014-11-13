1dc1bc9310f076aa1dc7a75312239f1658182b43
Vojtech Vitek (V-Teq) <vvitek@redhat.com>
2014-11-13 11:28:22 +0100

    Make TestMerge more readable

--- mergemaps_test.go
+++ mergemaps_test.go
@@ -9,69 +9,69 @@ import (
 
 func TestMerge(t *testing.T) {
 	testCases := []struct {
-		dst        interface{}
-		src        interface{}
-		flags      int
-		shouldPass bool
-		expected   interface{}
+		dst      interface{}
+		src      interface{}
+		flags    int
+		err      bool
+		expected interface{}
 	}{
 		{ // Test empty maps
-			map[int]int{},
-			map[int]int{},
-			0,
-			true,
-			map[int]int{},
+			dst:      map[int]int{},
+			src:      map[int]int{},
+			flags:    0,
+			err:      false,
+			expected: map[int]int{},
 		},
 		{ // Test dst + src => expected
-			map[int]string{1: "foo"},
-			map[int]string{2: "bar"},
-			0,
-			true,
-			map[int]string{1: "foo", 2: "bar"},
+			dst:      map[int]string{1: "foo"},
+			src:      map[int]string{2: "bar"},
+			flags:    0,
+			err:      false,
+			expected: map[int]string{1: "foo", 2: "bar"},
 		},
 		{ // Test dst + src => expected, do not overwrite dst
-			map[string]string{"foo": "bar"},
-			map[string]string{"foo": ""},
-			0,
-			true,
-			map[string]string{"foo": "bar"},
+			dst:      map[string]string{"foo": "bar"},
+			src:      map[string]string{"foo": ""},
+			flags:    0,
+			err:      false,
+			expected: map[string]string{"foo": "bar"},
 		},
 		{ // Test dst + src => expected, overwrite dst
-			map[string]string{"foo": "bar"},
-			map[string]string{"foo": ""},
-			mergemaps.OverwriteExistingDstKey,
-			true,
-			map[string]string{"foo": ""},
+			dst:      map[string]string{"foo": "bar"},
+			src:      map[string]string{"foo": ""},
+			flags:    mergemaps.OverwriteExistingDstKey,
+			err:      false,
+			expected: map[string]string{"foo": ""},
 		},
 		{ // Test dst + src => expected, error on existing key value
-			map[string]string{"foo": "bar"},
-			map[string]string{"foo": "bar"},
-			mergemaps.ErrorOnExistingDstKey | mergemaps.OverwriteExistingDstKey,
-			false,
-			map[string]string{"foo": "bar"},
+			dst:      map[string]string{"foo": "bar"},
+			src:      map[string]string{"foo": "bar"},
+			flags:    mergemaps.ErrorOnExistingDstKey | mergemaps.OverwriteExistingDstKey,
+			err:      true,
+			expected: map[string]string{"foo": "bar"},
 		},
 		{ // Test dst + src => expected, do not error on same key value
-			map[string]string{"foo": "bar"},
-			map[string]string{"foo": "bar"},
-			mergemaps.ErrorOnDifferentDstKeyValue | mergemaps.OverwriteExistingDstKey,
-			true,
-			map[string]string{"foo": "bar"},
+			dst:      map[string]string{"foo": "bar"},
+			src:      map[string]string{"foo": "bar"},
+			flags:    mergemaps.ErrorOnDifferentDstKeyValue | mergemaps.OverwriteExistingDstKey,
+			err:      false,
+			expected: map[string]string{"foo": "bar"},
 		},
 		{ // Test dst + src => expected, error on different key value
-			map[string]string{"foo": "bar"},
-			map[string]string{"foo": ""},
-			mergemaps.ErrorOnDifferentDstKeyValue | mergemaps.OverwriteExistingDstKey,
-			false,
-			map[string]string{"foo": "bar"},
+			dst:      map[string]string{"foo": "bar"},
+			src:      map[string]string{"foo": ""},
+			flags:    mergemaps.ErrorOnDifferentDstKeyValue | mergemaps.OverwriteExistingDstKey,
+			err:      true,
+			expected: map[string]string{"foo": "bar"},
 		},
 	}
 
 	for i, test := range testCases {
-		err := mergemaps.Merge(test.dst, test.src, test.flags)
-		if err != nil && test.shouldPass {
+		err := mergemaps.MergeInto(test.dst, test.src, test.flags)
+		if err != nil && !test.err {
 			t.Errorf("Unexpected error while merging § on testCase[%v].", i)
 		}
-		if err == nil && !test.shouldPass {
+		if err == nil && test.err {
 			t.Errorf("Unexpected non-error while merging maps on testCase[%v].", i)
 		}
 		if !reflect.DeepEqual(test.dst, test.expected) {
